df40b9ebc22d9691cfbe589adf5ed725e2d1c7d0
NEMYSESx <shakirraza4227@gmail.com>
2024-11-12 17:32:24 +0530

    fixing the layout

--- app/(root)/[userId]/page.tsx
+++ app/(root)/[userId]/page.tsx
@@ -9,15 +9,15 @@ const Dashboard = () => {
         <h1 className="font-semibold ml-12 text-6xl">Dashboard</h1>
         <div className="flex">
           <div>
-            <div className="h-[350px] w-[500px] bg-white rounded-2xl ml-12"></div>
+            <div className="h-[350px] w-[695px] bg-white rounded-2xl ml-12"></div>
             <div className="flex">
-              <div className="h-[250px] w-[300px] bg-white rounded-2xl ml-12 mt-5"></div>
-              <div className="w-[375px] h-[250px] bg-white rounded-2xl ml-5 mt-5"></div>
+              <div className="w-[337px] h-[360px] bg-white rounded-2xl ml-12 mt-5"></div>
+              <div className="w-[337px] h-[360px] bg-white rounded-2xl ml-5 mt-5"></div>
             </div>
           </div>
           <div>
-            <div className="w-[300px] h-[485px] bg-white rounded-2xl ml-5"></div>
-            <div className="w-[300px] h-[225px] bg-white rounded-2xl ml-5 mt-5"></div>
+            <div className="w-[420px] h-[485px] bg-white rounded-2xl ml-5"></div>
+            <div className="w-[420px] h-[225px] bg-white rounded-2xl ml-5 mt-5"></div>
           </div>
         </div>
       </div>
